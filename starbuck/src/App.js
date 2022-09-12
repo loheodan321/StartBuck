c8814c1b8549c1a512a1c179e8147b4ec02cef77
unknown <nguyenvotuanthanh0307@gmail.com>
2022-09-12 14:10:44 +0700

    Adding Skelecton App.js

--- starbuck/src/App.js
+++ starbuck/src/App.js
@@ -1,7 +1,16 @@
 import style from "./App.module.css";
+import Header from "./components/Header/Header";
 
 function App() {
-  return <div className={style.app}></div>;
+  return (
+    <div className={style["app"]}>
+      <div className={style["header"]}>
+        <Header />
+      </div>
+      <div className={style["section"]}></div>
+      <div className={style["footer"]}></div>
+    </div>
+  );
 }
 
 export default App;
