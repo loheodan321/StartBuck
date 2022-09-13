7ef3d1e5269860bab18603e42170171d69440d8f
unknown <nguyenvotuanthanh0307@gmail.com>
2022-09-13 12:02:44 +0700

    Fixing App.js and App.module.css, Adding Footer Skelecton and Styling

--- starbuck/src/App.js
+++ starbuck/src/App.js
@@ -1,17 +1,24 @@
 import style from "./App.module.css";
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Home from "./components/Section/Home/Home";
 
 function App() {
   return (
     <div className={style["app"]}>
-      <div className={style["header"]}>
-        <Header />
+      <div className={style["upper"]}>
+        <div className={style["header"]}>
+          <Header />
+        </div>
       </div>
-      <div className={style["section"]}>
-        <Home />
+      <div className={style["lower"]}>
+        <div className={style["section"]}>
+          <Home />
+        </div>
+        <div className={style["footer"]}>
+          <Footer />
+        </div>
       </div>
-      <div className={style["footer"]}></div>
     </div>
   );
 }
---
+++ starbuck/src/components/Footer/Footer.js
@@ -0,0 +1,25 @@
+import style from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <div className={style["footer"]}>
+      <div className={style["footer-contents"]}>
+        <h1>Starbuck Studio</h1>
+        <div className={style["address"]}>
+          A: 81 Le Van Duyet, Ward 3, Binh Thanh Distric, HCM City
+        </div>
+        <div className={style["telephone"]}>T: 0933237470</div>
+        <div className={style["email"]}>E: yourEmail@gmail.com</div>
+      </div>
+      <div className={style["footer-input"]}>
+        <form>
+          <input placeholder="Name" />
+          <input placeholder="Email" />
+          <button className={style["btn"]}>Subcribte now</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
